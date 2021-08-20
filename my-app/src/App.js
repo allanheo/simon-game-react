a7ba56ae36ffeeabae4af64aa50c2a5a9dcaebc1
Christine Chung <christinechung@Christines-MacBook-Air.local>
2021-08-20 02:17:47 -0500

    did some dumb stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import styled from 'styled-components';
 
+function App() {
+  
+  const Room = styled.div`
+  // position: relative;
+  // width: 500px;
+  // height: 500px;
+  // border: 10px solid black;
+  // margin: 0 auto;
+  `;
 
-const App = () => (
-    <Button />
-);
+  return (
+  <Room>
+    <Button type='button'/> 
+    <Button type='button'/>
+    <Button type='button'/><Button type='button'/><Button type='button'/>
+  </Room>  
+  );
+}
 
 export default App;
+
--- my-app/src/components/Button/index.js
+++ my-app/src/components/Button/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// Styles
+const Button = styled.button`
+  position: absolute;
+  background: white;
+  color: black;
+  border: 1px solid black;
+  border-radius: 10px;
+  width: 100px;
+  height: 50px;
+  cursor: pointer;
+`;
 
-const Button = () => (
-    <h1>button</h1>
+const LightSwitch = ({ callback, position, switchOn }) => (
+    <Button onClick={callback} position={position}>
+        <div>hello</div>
+    </Button>
 );
 
 export default Button;
